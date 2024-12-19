bdafa3b78036eaf5343d4ab4fe0c20e36160cfbf
safecyber <sabinafeenstra@gmail.com>
2024-12-19 20:23:31 +0100

    huiswerk les 6 django

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,27 +1,8 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
-from django.conf import settings
-
-
-# class Voetbalspelers(models.Model):
-#     naam = models.CharField(max_length=100)
-#     actuele_club = models.CharField(max_length=100)
-#     auteur = models.ForeignKey(User, on_delete=models.CASCADE)
-#     datum_invoer = models.DateTimeField(default=timezone.now)
-#     datum_laatste_aanpassing = models.DateTimeField(auto_now=True)
-#     leeftijd = models.IntegerField()
-#     positie = models.CharField(max_length=50)
-
-#     def save(self, *args, **kwargs):
-#         # Hier kun je extra acties uitvoeren vóór het opslaan
-#         super().save(*args, **kwargs)
-
-#     def __str__(self):
-#         return self.naam
 
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
@@ -29,7 +10,7 @@ class Post(models.Model):
 
     def publish(self):
         self.published_date = timezone.now()
-        self.save
+        self.save()
 
     def __str__(self):
         return self.title
--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.post_list, name='post_list'),  # Hoofdpagina (lijst van posts)
+    path('post/<int:pk>/', views.post_detail, name='post_inhoud'),  # Detailpagina van een post
+    path('post/nieuw/', views.post_nieuw, name='post_nieuw'),  # Pagina voor nieuwe post
 ]
